3b77b8feed62db77b2dcfe7d800deb76cf976b63
Morgan MDanat <mdanatmorgan3@gmail.com>
2023-03-30 17:08:04 -0600

    array

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
-// Write your solution here!
+
+
+const cats = ["Milo","Otis","Garfield"];
+const newCats = [...cats, "Broom"] 
+const removedCats = ["Arnold", ...cats ]
+
+function destructivelyAppendCat(name){
+    cats.push(name)
+}
+//console.log(destructivelyAppendCat("Ralph"));
+
+function destructivelyPrependCat(name){
+    cats.unshift(name)
+}
+//console.log(destructivelyPrependCat("Bob"));
+
+
+function destructivelyRemoveLastCat(){
+    cats.pop()
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift()
+}
+
+
+
+function appendCat(name){
+  //const newCats = [...cats, "Broom"] -- can you add const to a function 
+    return newCats 
+}
+
+function prependCat(name){
+    return removedCats
+}
+
+function removeLastCat(){
+    const lastCat = [...cats];
+    lastCat.splice(2,1);
+    return lastCat
+}
+ 
+
+function removeFirstCat(){
+    const firstCat = [...cats];
+    firstCat.splice(0,1);
+    return firstCat
+}
+
